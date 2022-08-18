6f1bfb725173a7e577738f04658686e803753987
lukas <mail@lukas-eisenhauer.de>
2022-08-18 21:46:27 +0200

    fix issue where the chord preview in the play section would not update when the first phrase is entered

--- app/src/main/java/com/lukas/music/song/chords/ChordProgression.kt
+++ app/src/main/java/com/lukas/music/song/chords/ChordProgression.kt
@@ -2,4 +2,14 @@ package com.lukas.music.song.chords
 
 import com.lukas.music.util.MetaCycle
 
-class ChordProgression : MetaCycle<Phrase>()
+class ChordProgression : MetaCycle<Phrase>() {
+    override fun add(element: Phrase): Boolean {
+        if (size == 0) {
+            for (callback in stepCallback) {
+                // first step
+                callback()
+            }
+        }
+        return super.add(element)
+    }
+}

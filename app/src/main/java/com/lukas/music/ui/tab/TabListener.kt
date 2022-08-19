9a419077886bcb5059e7d495ae7111eb92260668
lukas <mail@lukas-eisenhauer.de>
2022-08-19 11:01:07 +0200

    add GPL v3 copyright

--- app/src/main/java/com/lukas/music/ui/tab/TabListener.kt
+++ app/src/main/java/com/lukas/music/ui/tab/TabListener.kt
@@ -1,9 +1,19 @@
+/*
+ * Copyright (C) 2022 Lukas Eisenhauer
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.lukas.music.ui.tab
 
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 
-class TabListener(private val pager: ViewPager2): TabLayout.OnTabSelectedListener {
+class TabListener(private val pager: ViewPager2) : TabLayout.OnTabSelectedListener {
     override fun onTabSelected(tab: TabLayout.Tab?) {
         tab?.let { pager.currentItem = it.position }
     }
